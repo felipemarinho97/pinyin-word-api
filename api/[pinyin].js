9faa8c5ec95ff5ee9f947ef02e344126c3476ded
Felipe Marinho <felipevm97@gmail.com>
2020-09-04 18:17:16 -0300

    allow cross origin

--- api/[pinyin].js
+++ api/[pinyin].js
@@ -3,7 +3,25 @@ const axios = require("axios");
 const YABLA_URI =
   "https://chinese.yabla.com/chinese-english-pinyin-dictionary.php";
 
-export default (req, res) => {
+const allowCors = (fn) => async (req, res) => {
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
+  return await fn(req, res);
+};
+
+export default allowCors((req, res) => {
   const {
     query: { pinyin },
   } = req;
@@ -11,4 +29,4 @@ export default (req, res) => {
   axios.get(`${YABLA_URI}?define=${encodeURI(pinyin)}`).then((_res) => {
     res.json({ url: (_res.data + "").match(/https.*\.mp3/)[0] });
   });
-};
+});
